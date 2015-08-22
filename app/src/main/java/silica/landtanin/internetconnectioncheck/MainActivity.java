2258694c6d33e67f2060bb6e31a140b2f4886ed0
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-08-22 16:24:57 +0700

    finish 1

--- app/src/main/java/silica/landtanin/internetconnectioncheck/MainActivity.java
+++ app/src/main/java/silica/landtanin/internetconnectioncheck/MainActivity.java
@@ -1,16 +1,68 @@
 package silica.landtanin.internetconnectioncheck;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button checkNetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bindwidget
+        bindwidget();
+
+        //Check Online Method
+        isOnline();
+
+        //button controller
+        buttonController();
+    }
+
+    private void bindwidget() {
+
+        checkNetButton = (Button) findViewById(R.id.button);
+
+    }
+
+    private void buttonController() {
+        checkNetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (isOnline() == true) {
+                    Toast.makeText(MainActivity.this, "Connected", 5000).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Connection Failed", 5000).show();
+                }
+            }
+        });
+    }
+
+    private boolean isOnline() {
+
+        Boolean result = false;
+
+        ConnectivityManager objConnectManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo objNetWorkInfo = objConnectManager.getActiveNetworkInfo();
+
+        if (objNetWorkInfo != null && objNetWorkInfo.isConnected()) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
     }
 
     @Override
